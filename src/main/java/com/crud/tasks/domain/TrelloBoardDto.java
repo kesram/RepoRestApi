b157bea50a07189a14fc86e0edd8eff7aa56e0d8
Marcin Sęk <marcin.sek123@gmail.com>
2022-10-26 23:44:00 +0200

    22.3. Rozbudowa klienta,Zadanie: rozszerzenie klasy odpowiedzi

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,15 +1,28 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 //@Getter
 //@AllArgsConstructor
 //@NoArgsConstructor
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
+
 }
