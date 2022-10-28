ab50195ac511c7df048b74df490d6d58e15ef2c5
Marcin Sęk <marcin.sek123@gmail.com>
2022-10-28 19:01:03 +0200

    23.3. Wysyłanie maili z aplikacji, Zadanie: dodanie maila do wiadomości

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,11 +1,14 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -14,16 +17,27 @@ public class SimpleEmailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void send(final String receiverEmail, final String subject, final String message) {
+    public void send(final Mail mail) {
+        log.info("Starting email preparation...");
         try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(receiverEmail);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(message);
-
+            SimpleMailMessage mailMessage = createMailMessage(mail);
             javaMailSender.send(mailMessage);
+            log.info("Email has been sent.");
         } catch (MailException e){
             log.error("Failed to process email sending: " + e.getMessage(), e);
         }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        Optional<String> optionalToCc = Optional.ofNullable(mail.getToCc());
+        if (optionalToCc.isPresent()) {
+            mailMessage.setCc(mail.getToCc());
+            log.info("CC is present");
+        }
+        return mailMessage;
     }
 }
