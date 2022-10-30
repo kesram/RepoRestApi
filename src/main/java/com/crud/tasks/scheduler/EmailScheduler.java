0d916d30777e730a46dace5e00ab667d6bc9adea
Marcin Sęk <marcin.sek123@gmail.com>
2022-10-30 18:04:43 +0100

    IF in one line

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -24,17 +24,9 @@ public class EmailScheduler {
                 new Mail(
                         adminConfig.getAdminMail(),
                         SUBJECT,
-                        messageToSent(),
+                        "Currently in database you got: " + size +  (size == 1 ?" task":" tasks"),
                         null
                 )
         );
-    }
-    public String messageToSent(){
-        String message = null;
-        long size =taskRepository.count();
-        if(size==1){
-           message = "Currently in database you got: " + size + " task";
-        } else message = "Currently in database you got: " + size + " tasks";
-        return message;
     }
 }
